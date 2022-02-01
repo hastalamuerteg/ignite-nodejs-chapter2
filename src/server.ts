311f22a2b47eaec83d45a803dafc9ee80f9a1147
hastalamuerteg <gabriel.vicent@hotmail.com>
2022-01-31 22:04:30 -0300

    init swagger doc

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { router } from './routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from '../src/swagger.json'
 
 const app = express();
 app.use(express.json());
 app.use(router)
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.listen(3001, () => {
   console.log('Server is running');
 })
