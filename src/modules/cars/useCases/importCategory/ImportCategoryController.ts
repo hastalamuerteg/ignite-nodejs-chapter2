825c03cee449720fed19f71c7d2b6d4d3537719c
hastalamuerteg <gabriel.vicent@hotmail.com>
2022-02-16 09:04:41 -0300

    added migration users + swagger + accounts module

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,15 +1,14 @@
-import { Request, Response } from 'express'
-import { ImportCategoryUseCase } from './ImportCategoryUseCase'
-import { container } from 'tsyringe'
+import { Request, Response } from "express";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+import { container } from "tsyringe";
 
 class ImportCategoryController {
-  
   async handle(req: Request, res: Response) {
-    const importCategoryUseCase = container.resolve(ImportCategoryUseCase)
+    const importCategoryUseCase = container.resolve(ImportCategoryUseCase);
     const { file } = req;
-    await importCategoryUseCase.execute(file)
-    return res.send()
+    await importCategoryUseCase.execute(file);
+    return res.status(201).send();
   }
 }
 
-export { ImportCategoryController }
+export { ImportCategoryController };
